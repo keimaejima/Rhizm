324b161baf93eb1e1c228ed796bb6579c87d77a1
keimaejima <souloasis0@gmail.com>
2018-11-07 17:23:51 +0900

    メーラーまわりの設定を追加

--- lib/api/v1/stripe_api.rb
+++ lib/api/v1/stripe_api.rb
@@ -8,30 +8,35 @@ class Stripe_API < Grape::API
    #   requires :challenge
    # end
   post do
-      user = User.create(id: User.last.id + 1, email: params[:stripeEmail])
-      stripe_customer = Stripe::Customer.create(
-        email:       params[:stripeEmail],
-        metadata: {
-          user_id: user.id,
-          rails_env: Rails.env.to_s
-        } # デバッグや管理のためmetadataを任意に追加できる
-      )
+      begin
+        user = User.create(id: User.last.id + 1, email: params[:stripeEmail])
+        stripe_customer = Stripe::Customer.create(
+          email:       params[:stripeEmail],
+          metadata: {
+            user_id: user.id,
+            rails_env: Rails.env.to_s
+          } # デバッグや管理のためmetadataを任意に追加できる
+        )
 
-      stripe_customer = Stripe::Customer.retrieve(stripe_customer.id)
-      stripe_customer.source = params[:stripeToken] # Stripe.jsで変換したトークンを渡すだけ
-      stripe_customer.save
+        stripe_customer = Stripe::Customer.retrieve(stripe_customer.id)
+        stripe_customer.source = params[:stripeToken] # Stripe.jsで変換したトークンを渡すだけ
+        stripe_customer.save
 
-      # stripe_subscription = Stripe::Subscription.retrieve(stripe_customer.id)
-      # stripe_subscription.plan = Plan.find(params[:plan_id])
-      # stripe_subscription.save
-      Stripe::Subscription.create(
-        :customer => stripe_customer.id,
-        :items => [
-          {
-            :plan => params[:plan_id],
-          },
-        ]
-      )
+        # stripe_subscription = Stripe::Subscription.retrieve(stripe_customer.id)
+        # stripe_subscription.plan = Plan.find(params[:plan_id])
+        # stripe_subscription.save
+        Stripe::Subscription.create(
+          :customer => stripe_customer.id,
+          :items => [
+            {
+              :plan => params[:plan_id],
+            },
+          ]
+        )
+      rescue => e
+        Rails.logger.fatal(Settings.alert.occured_in_api)
+        Rails.logger.fatal(e)
+      end
   end
 end
 end
